28e5720f36f2c00d26344f88d129f181de14ae35
William Wnekowicz <william.wnekowicz@gmail.com>
2014-09-06 23:17:14 -0700

    Comments out everything in order to focus on alpha waves

--- js/rickshaw_viz.js
+++ js/rickshaw_viz.js
@@ -1,7 +1,7 @@
  var palette = new Rickshaw.Color.Palette({ scheme: 'spectrum14' });
 
 var time_value = 250;
-var max_data_points = 10;
+var max_data_points = 100;
 var time_base = new Date().getTime() / 1000;
 
 
@@ -10,6 +10,7 @@ var eeg_graph = new Rickshaw.Graph({
 	width: 900,
 	height: 500,
 	renderer: 'line',
+
 	series: new Rickshaw.Series.FixedDuration([{name: 'default'}], undefined, {
 		timeInterval: time_value,
 		maxDataPoints: max_data_points,
@@ -28,8 +29,8 @@ var eeg_y_axis = new Rickshaw.Graph.Axis.Y({
 eeg_graph.render();
 
 var new_data = {};
-socket.on('/muse/eeg', function(data){
-	
+socket.on('/muse/dsp/elements/alpha', function(data){
+
 	new_data[0] = data[0].value
 	new_data[1] = data[1].value
 	new_data[2] = data[2].value
--- js/tabular_data.js
+++ js/tabular_data.js
@@ -1,78 +1,78 @@
-socket.on('/muse/eeg', function (data) {
-    document.getElementById('eeg0').innerHTML = data[0].value;
-    document.getElementById('eeg1').innerHTML = data[1].value;
-    document.getElementById('eeg2').innerHTML = data[2].value;
-    document.getElementById('eeg3').innerHTML = data[3].value;
-});
-socket.on('/muse/eeg/quantization', function (data) {
-    document.getElementById('q0').innerHTML = data[0].value;
-    document.getElementById('q1').innerHTML = data[1].value;
-    document.getElementById('q2').innerHTML = data[2].value;
-    document.getElementById('q3').innerHTML = data[3].value;
-});
-socket.on('/muse/acc', function(data){
-    document.getElementById('acc0').innerHTML = data[0].value;
-    document.getElementById('acc1').innerHTML = data[1].value;
-    document.getElementById('acc2').innerHTML = data[2].value;
-});
-socket.on('/muse/drlref', function(data){
-    document.getElementById('drlref0').innerHTML = data[0].value;
-    document.getElementById('drlref1').innerHTML = data[1].value;
-});
-socket.on('/muse/dsp/elements/low_freqs', function (data) {
-    document.getElementById('low_freqs0').innerHTML = data[0].value;
-    document.getElementById('low_freqs1').innerHTML = data[1].value;
-    document.getElementById('low_freqs2').innerHTML = data[2].value;
-    document.getElementById('low_freqs3').innerHTML = data[3].value;
-});
+// socket.on('/muse/eeg', function (data) {
+//     document.getElementById('eeg0').innerHTML = data[0].value;
+//     document.getElementById('eeg1').innerHTML = data[1].value;
+//     document.getElementById('eeg2').innerHTML = data[2].value;
+//     document.getElementById('eeg3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/eeg/quantization', function (data) {
+//     document.getElementById('q0').innerHTML = data[0].value;
+//     document.getElementById('q1').innerHTML = data[1].value;
+//     document.getElementById('q2').innerHTML = data[2].value;
+//     document.getElementById('q3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/acc', function(data){
+//     document.getElementById('acc0').innerHTML = data[0].value;
+//     document.getElementById('acc1').innerHTML = data[1].value;
+//     document.getElementById('acc2').innerHTML = data[2].value;
+// });
+// socket.on('/muse/drlref', function(data){
+//     document.getElementById('drlref0').innerHTML = data[0].value;
+//     document.getElementById('drlref1').innerHTML = data[1].value;
+// });
+// socket.on('/muse/dsp/elements/low_freqs', function (data) {
+//     document.getElementById('low_freqs0').innerHTML = data[0].value;
+//     document.getElementById('low_freqs1').innerHTML = data[1].value;
+//     document.getElementById('low_freqs2').innerHTML = data[2].value;
+//     document.getElementById('low_freqs3').innerHTML = data[3].value;
+// });
 socket.on('/muse/dsp/elements/alpha', function (data) {
     document.getElementById('alpha0').innerHTML = data[0].value;
     document.getElementById('alpha1').innerHTML = data[1].value;
     document.getElementById('alpha2').innerHTML = data[2].value;
     document.getElementById('alpha3').innerHTML = data[3].value;
 });
-socket.on('/muse/dsp/elements/beta', function (data) {
-    document.getElementById('beta0').innerHTML = data[0].value;
-    document.getElementById('beta1').innerHTML = data[1].value;
-    document.getElementById('beta2').innerHTML = data[2].value;
-    document.getElementById('beta3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/delta', function (data) {
-    document.getElementById('delta0').innerHTML = data[0].value;
-    document.getElementById('delta1').innerHTML = data[1].value;
-    document.getElementById('delta2').innerHTML = data[2].value;
-    document.getElementById('delta3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/gamma', function (data) {
-    document.getElementById('gamma0').innerHTML = data[0].value;
-    document.getElementById('gamma1').innerHTML = data[1].value;
-    document.getElementById('gamma2').innerHTML = data[2].value;
-    document.getElementById('gamma3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/theta', function (data) {
-    document.getElementById('theta0').innerHTML = data[0].value;
-    document.getElementById('theta1').innerHTML = data[1].value;
-    document.getElementById('theta2').innerHTML = data[2].value;
-    document.getElementById('theta3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/horseshoe', function (data) {
-    document.getElementById('horseshoe0').innerHTML = data[0].value;
-    document.getElementById('horseshoe1').innerHTML = data[1].value;
-    document.getElementById('horseshoe2').innerHTML = data[2].value;
-    document.getElementById('horseshoe3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/is_good', function (data) {
-    document.getElementById('is_good0').innerHTML = data[0].value;
-    document.getElementById('is_good1').innerHTML = data[1].value;
-    document.getElementById('is_good2').innerHTML = data[2].value;
-    document.getElementById('is_good3').innerHTML = data[3].value;
-});
-socket.on('/muse/dsp/elements/blink', function (data) {
-    document.getElementById('blink').innerHTML = data[0].value;
-});
-socket.on('/muse/dsp/elements/jaw_clench', function (data) {
-    document.getElementById('jaw_clench').innerHTML = data[0].value;
-});
-socket.on('/muse/dsp/elements/touching_forehead', function (data) {
-    document.getElementById('touching_forehead').innerHTML = data[0].value;
-}); 
+// socket.on('/muse/dsp/elements/beta', function (data) {
+//     document.getElementById('beta0').innerHTML = data[0].value;
+//     document.getElementById('beta1').innerHTML = data[1].value;
+//     document.getElementById('beta2').innerHTML = data[2].value;
+//     document.getElementById('beta3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/delta', function (data) {
+//     document.getElementById('delta0').innerHTML = data[0].value;
+//     document.getElementById('delta1').innerHTML = data[1].value;
+//     document.getElementById('delta2').innerHTML = data[2].value;
+//     document.getElementById('delta3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/gamma', function (data) {
+//     document.getElementById('gamma0').innerHTML = data[0].value;
+//     document.getElementById('gamma1').innerHTML = data[1].value;
+//     document.getElementById('gamma2').innerHTML = data[2].value;
+//     document.getElementById('gamma3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/theta', function (data) {
+//     document.getElementById('theta0').innerHTML = data[0].value;
+//     document.getElementById('theta1').innerHTML = data[1].value;
+//     document.getElementById('theta2').innerHTML = data[2].value;
+//     document.getElementById('theta3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/horseshoe', function (data) {
+//     document.getElementById('horseshoe0').innerHTML = data[0].value;
+//     document.getElementById('horseshoe1').innerHTML = data[1].value;
+//     document.getElementById('horseshoe2').innerHTML = data[2].value;
+//     document.getElementById('horseshoe3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/is_good', function (data) {
+//     document.getElementById('is_good0').innerHTML = data[0].value;
+//     document.getElementById('is_good1').innerHTML = data[1].value;
+//     document.getElementById('is_good2').innerHTML = data[2].value;
+//     document.getElementById('is_good3').innerHTML = data[3].value;
+// });
+// socket.on('/muse/dsp/elements/blink', function (data) {
+//     document.getElementById('blink').innerHTML = data[0].value;
+// });
+// socket.on('/muse/dsp/elements/jaw_clench', function (data) {
+//     document.getElementById('jaw_clench').innerHTML = data[0].value;
+// });
+// socket.on('/muse/dsp/elements/touching_forehead', function (data) {
+//     document.getElementById('touching_forehead').innerHTML = data[0].value;
+// }); 
