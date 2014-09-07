d168df54f4cb4faeffc2b58021171a0819ace209
cimbriano <Christopher.Imbriano@gmail.com>
2014-09-06 17:37:30 -0700

    Adding package and express

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
+var socket = require('socket.io'),
+	express = require('express'),
+	app = express.createServer(),
+	io = socket.listen(app);
 
 app.listen(8080);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-
-}
+app.get('/', function(req, response){
+	response.sendfile(__dirname + "/index.html");
+});
 
 
 io.on('connection', function (socket) {
   console.log('Connection received.');
-
 
   socket.on('oscdata', function(oscdata){
     console.log(oscdata);
